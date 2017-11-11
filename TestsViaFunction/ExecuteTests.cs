9221cfcfaf84c777f9786aed5f75bfa27eab396e
Dariusz Parzygnat <dariusz.parzygnat@gmail.com>
2017-11-11 20:18:11 +0100

    Resolved problem with casting proxy by runner

--- TestsViaFunction/ExecuteTests.cs
+++ TestsViaFunction/ExecuteTests.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Configuration;
 using System.IO;
+using System.Reflection;
 using Xunit.Runners;
 using System.Threading;
 using Microsoft.Azure.WebJobs;
@@ -11,13 +14,10 @@ namespace TestsViaFunction
         [FunctionName("ExecuteTests")]
         public static void Run([QueueTrigger("queue-with-tests", Connection = "storage_Conn")]TestTypeInfo myQueueItem, [Table(tableName: "TestResults", Connection = "storage_Conn")] ICollector<TestResult> outputTable, TraceWriter log)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
             ManualResetEvent executionManualResetEvent = new ManualResetEvent(false);
             var resultProxy = new ResultProxy(outputTable, myQueueItem);
-            var fileExists = File.Exists(myQueueItem.DllPath);
-            log.Info(fileExists.ToString());
-            log.Info(myQueueItem.DllPath);
             var runner = AssemblyRunner.WithAppDomain(myQueueItem.DllPath);
-            log.Info("Runner created");
             runner.OnTestFailed += resultProxy.OnTestFailed;
             runner.OnTestPassed += resultProxy.OnTestPassed;
             runner.OnTestSkipped += resultProxy.OnTestSkipped;
@@ -32,6 +32,21 @@ namespace TestsViaFunction
                 Thread.Sleep(10);
             }
             runner.Dispose();
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string projectDir = System.Environment.GetEnvironmentVariable("DLL_Location", EnvironmentVariableTarget.Process);
+            int commaIndex = args.Name.IndexOf(',');
+            string shortAssemblyName = commaIndex == -1? args.Name : args.Name.Substring(0, commaIndex);
+            string fileName = Path.Combine(projectDir, shortAssemblyName + ".dll");
+            if (File.Exists(fileName))
+            {
+                Assembly result = Assembly.LoadFrom(fileName);
+                return result;
+            }
+            else
+                return Assembly.GetExecutingAssembly().FullName == args.Name ? Assembly.GetExecutingAssembly() : null;
         }
     }
 
